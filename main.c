f5a9df57c3801212dc742baaa9bc004710643ed1
YEBOAH-DONSAH GODFRED <80332397+donsah@users.noreply.github.com>
2024-04-09 21:11:44 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -27,7 +27,7 @@
 //You can get these value from the datasheet of servo you use, in general pulse width varies between 1000 to 2000 mocrosecond
 #define SERVO_MIN_PULSEWIDTH 1000 //Minimum pulse width in microsecond
 #define SERVO_MAX_PULSEWIDTH 2000 //Maximum pulse width in microsecond
-#define SERVO_MAX_DEGREE 180 //Maximum angle in degree upto which servo can rotate
+#define SERVO_MAX_DEGREE 90 //Maximum angle in degree upto which servo can rotate
 
 // #define UNINITIALIZED_VALUE -999
 
